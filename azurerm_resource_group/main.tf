1d29f66d9c3c30157dac3b47c70f72061dc657c5
aviansh771 <avinash122023@gmail.com>
2025-06-30 17:47:00 +0530

    output block add

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -10,3 +10,8 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
 }
+
+output "ip" {
+  value = azurerm_virtual_network.virtual_network.id
+  
+}
